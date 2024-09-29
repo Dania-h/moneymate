eeb4eb993141fb4e8f66f761b45b135251af74c1
Dania <dabeaher@yahoo.com>
2024-09-29 05:43:15 -0400

    set up new backend with node and express

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const corsOptions = {
+    origin: ["http://localhost:5173"]
+};
+
+app.get("/api", (req, res) => {
+    res.json({ });
+});
+
+app.listen(8080, () => {
+    console.log("Server started on port 8080");
+});
