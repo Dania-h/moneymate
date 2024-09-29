24b034f45be2027d48f95931d31385af33280579
Dania <dabeaher@yahoo.com>
2024-09-29 07:54:21 -0400

    the buddy card map through data instead of being hardcoded

--- frontend/src/BuddyCard/BuddyCard.jsx
+++ frontend/src/BuddyCard/BuddyCard.jsx
@@ -1,22 +1,33 @@
-import otherprofile from "../assets/profile1.png";
+import PropTypes from "prop-types";
 import Button from "../Button/Button";
 
-function BuddyCard() {
+function BuddyCard({ username, picture, bio, financialGoals }) {
   return (
     <div className="card">
       <div className="pic-name">
-        <img className="profile-pic" src={otherprofile} />
-        <h2>JaneyJane</h2>
+        <img className="profile-pic" src={picture} />
+        <h2>{username}</h2>
       </div>
       <div>
         <h3>Bio</h3>
-        <p>Hi everyone! I need some help staying on track to save up money for a home.</p>
+        <p>{bio}</p>
         <h3>Financial Goals</h3>
-        <p>Home Buying</p>
+        <ul>
+          {financialGoals.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
         <Button text="Request a match" />
       </div>
     </div>
   );
 }
+
+BuddyCard.propTypes = {
+  username: PropTypes.string.isRequired,
+  picture: PropTypes.node.isRequired,
+  bio: PropTypes.string,
+  financialGoals: PropTypes.array,
+};
 
 export default BuddyCard;
--- frontend/src/Find.jsx
+++ frontend/src/Find.jsx
@@ -1,9 +1,40 @@
 import BuddyCard from "./BuddyCard/BuddyCard";
+import profile1 from "./assets/profile1.png";
+import profile2 from "./assets/profile2.png";
 
 function Find() {
+  const buddies = [
+    {
+      id: 1,
+      username: "JaneyJane",
+      picture: profile1,
+      language: "English",
+      bio: "Hi everyone! I need some help staying on track to save up money for a home.",
+      financialGoals: ["Home Buying", "Emergency Fund"],
+    },
+    {
+      id: 2,
+      username: "HombreCash",
+      picture: profile2,
+      language: "Spanish",
+      bio: "hola gente necesito ayuda porque me quedo sin dinero cada mes",
+      financialGoals: ["Budgeting"],
+    },
+  ];
+
   return (
-    <section style={{ padding: "1.5rem 6rem" }}>
-      <BuddyCard />
+    <section style={{ padding: "1.5rem 6rem", display: "flex" }}>
+      {buddies.map((buddy) => {
+        return (
+          <BuddyCard
+            key={buddy.id}
+            username={buddy.username}
+            picture={buddy.picture}
+            bio={buddy.bio}
+            financialGoals={buddy.financialGoals}
+          />
+        );
+      })}
     </section>
   );
 }
